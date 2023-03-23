0229082fba4563fd3e62c0a88918273acfa0a0f8
José Manuel Cornelio <batcornelio@gmail.com>
2023-03-23 15:42:45 -0400

    Generation of basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    NavigationBarComponent,
+    LandingPageComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
